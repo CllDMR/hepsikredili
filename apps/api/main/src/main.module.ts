25c8233b4578c1ef2c36cc1849664e8316cbb6f2
CllDMR <celal.demirx@gmail.com>
2022-01-15 18:41:30 +0300

    moved app module into main module

--- apps/api/main/src/main.module.ts
+++ apps/api/main/src/main.module.ts
@@ -12,8 +12,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ThrottlerModule } from '@nestjs/throttler';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -54,7 +52,5 @@ import { AppService } from './app.service';
     ApiMainSharedModule,
     ApiMainUserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class ApiMainMainModule {}
