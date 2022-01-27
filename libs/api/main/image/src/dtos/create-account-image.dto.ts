a25b26516c262d5f89278760d95f8f36628d83fa
CllDMR <celal.demirx@gmail.com>
2022-01-27 21:35:59 +0300

    format file

--- libs/api/main/image/src/dtos/create-account-image.dto.ts
+++ libs/api/main/image/src/dtos/create-account-image.dto.ts
@@ -3,8 +3,10 @@ import { IsString } from 'class-validator';
 export class CreateAccountImageDto {
   @IsString()
   url!: string;
+
   @IsString()
   cloudinaryId!: string;
+
   @IsString()
   owner!: string;
 }
