07bbd130f8072b642013cacc02c32f069c8ccc59
CllDMR <celal.demirx@gmail.com>
2022-02-02 20:46:17 +0300

    added api-main-shared types

---
+++ libs/api/main/shared/src/typings/ad.types.ts
@@ -0,0 +1,26 @@
+export enum PaymentEnum {
+  NotDefined = 0,
+  Satılık = 1,
+  Kiralık = 2,
+  DevrenSatılık = 3,
+  DevrenKiralık = 4,
+}
+
+export enum IsıtmaEnum {
+  DoğalgazKombi = 0,
+  FancoilÜnitesi = 1,
+  Soba = 2,
+}
+
+export enum KullanımDurumuEnum {
+  Boş = 0,
+}
+
+export enum TapuDurumuEnum {
+  KatMülkiyetli = 0,
+}
+
+export enum KimdenEnum {
+  EmlakOfisinden = 0,
+  Sahibinden = 1,
+}
