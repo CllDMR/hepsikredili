db9b012090d5f524486cda19243cd7e80b5580a5
CllDMR <celal.demirx@gmail.com>
2022-02-09 21:20:08 +0300

    created shared button ui

---
+++ libs/web/shared/ui/src/button/contained/contained.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+import { ButtonBaseProps } from '..';
+import './contained.module.css';
+
+export interface ButtonContainedProps extends ButtonBaseProps {
+  variant: 'contained';
+  buttonRef?: React.ForwardedRef<HTMLButtonElement>;
+}
+
+export const ButtonContained: FC<ButtonContainedProps> = ({
+  title,
+  fullWidth = false,
+  buttonRef,
+  onClick,
+}) => (
+  <button
+    className={`disabled:bg-gray-500 text-white font-semibold hover:bg-indigo-600
+      min-w-[128px] border transition duration-150 ease-in-out 
+      border-gray-300 focus:outline-none focus-visible:ring-2 
+      focus-visible:ring-indigo-500 focus-visible:ring-opacity-75 px-4 py-2 bg-indigo-500 rounded ${
+        fullWidth && 'w-full'
+      }`}
+    ref={buttonRef}
+    onClick={onClick}
+  >
+    <span className="whitespace-nowrap">{title}</span>
+  </button>
+);
+
+export default ButtonContained;
---
+++ libs/web/shared/ui/src/button/outlined/outlined.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+import { ButtonBaseProps } from '..';
+import './outlined.module.css';
+
+export interface ButtonOutlinedProps extends ButtonBaseProps {
+  variant: 'outlined';
+  buttonRef?: React.ForwardedRef<HTMLButtonElement>;
+}
+
+export const ButtonOutlined: FC<ButtonOutlinedProps> = ({
+  title,
+  fullWidth = false,
+  buttonRef,
+  onClick,
+}) => (
+  <button
+    className={`disabled:bg-gray-500 text-indigo-400 font-semibold hover:bg-indigo-200
+      min-w-[128px] border transition duration-150 ease-in-out 
+      border-indigo-500 focus:outline-none focus-visible:ring-2 
+      focus-visible:ring-indigo-500 focus-visible:ring-opacity-75 px-4 py-2 rounded ${
+        fullWidth && 'w-full'
+      }`}
+    ref={buttonRef}
+    onClick={onClick}
+  >
+    <span className="whitespace-nowrap">{title}</span>
+  </button>
+);
+
+export default ButtonOutlined;
