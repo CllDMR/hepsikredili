c41ae647b4fbb9d63c5a8573c73d2da284229c22
CllDMR <celal.demirx@gmail.com>
2022-02-12 08:04:10 +0300

    fix ignore build

--- tools/web-customer.ignore-vercel-build.sh
+++ tools/web-customer.ignore-vercel-build.sh
@@ -7,14 +7,8 @@ NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nr
 TS_VERSION=$(node -e "console.log(require('./package.json').devDependencies['typescript'])")
 
 # Install @nrwl/workspace in order to run the affected command
-echo "yarn add start"
-yarn add -D --prefer-offline --frozen-lockfile "@nrwl/workspace@$(cat ./package.json | jq -r '.devDependencies["@nrwl/workspace"]')"
-yarn add -D --prefer-offline --frozen-lockfile "typescript@$(cat ./package.json | jq -r '.devDependencies["typescript"]')"
-# yarn add -D --prefer-offline --frozen-lockfile "@nrwl/workspace@$NX_VERSION@$(cat ./package.json | jq -r '.devDependencies["@nrwl/workspace@$NX_VERSION"]')"
-# yarn add -D --prefer-offline --frozen-lockfile "typescript@$TS_VERSION@$(cat ./package.json | jq -r '.devDependencies["typescript@$TS_VERSION"]')"
-# yarn add -D @nrwl/workspace@$NX_VERSION --prefer-offline --frozen-lockfile
-# yarn add -D typescript@$TS_VERSION --prefer-offline --frozen-lockfile
-echo "yarn add end"
+yarn add -D @nrwl/workspace@$NX_VERSION --prefer-offline --frozen-lockfile
+yarn add -D typescript@$TS_VERSION --prefer-offline --frozen-lockfile
 
 # Run the affected command, comparing latest commit to the one before that
 npx nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
