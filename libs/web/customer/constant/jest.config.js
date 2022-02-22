fb8caffd178d82c58bb5d460289a086e311dbac1
CllDMR <celal.demirx@gmail.com>
2022-02-22 18:35:12 +0300

    implemented web customer constants lib

---
+++ libs/web/customer/constant/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'web-customer-constant',
+  preset: '../../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../../coverage/libs/web/customer/constant',
+};
---
+++ libs/web/customer/constant/src/enums.ts
@@ -0,0 +1,26 @@
+export enum PaymentEnum {
+  NotDefined,
+  Satılık,
+  Kiralık,
+  DevrenSatılık,
+  DevrenKiralık,
+}
+
+export enum IsıtmaEnum {
+  DoğalgazKombi,
+  FancoilÜnitesi,
+  Soba,
+}
+
+export enum KullanımDurumuEnum {
+  Boş,
+}
+
+export enum TapuDurumuEnum {
+  KatMülkiyetli,
+}
+
+export enum KimdenEnum {
+  EmlakOfisinden,
+  Sahibinden,
+}
