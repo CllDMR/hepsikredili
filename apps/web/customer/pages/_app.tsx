4265a04942ebe2f17781bdabfac6fded52516da1
CllDMR <celal.demirx@gmail.com>
2022-02-23 17:40:26 +0300

    implemented react-query wrapper onto web-customer app

--- apps/web/customer/pages/_app.tsx
+++ apps/web/customer/pages/_app.tsx
@@ -1,8 +1,32 @@
 import { AppProps } from 'next/app';
+import { useRef } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { Hydrate } from 'react-query/hydration';
 import './index.css';
 
-const CustomApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const CustomApp = ({ Component, pageProps }: AppProps) => {
+  const queryClientRef = useRef<undefined | QueryClient>();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: process.env.NODE_ENV === 'production',
+          refetchOnWindowFocus: process.env.NODE_ENV === 'production',
+          // staleTime: Infinity,
+        },
+      },
+    });
+  }
+
+  return (
+    <QueryClientProvider client={queryClientRef.current}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
+};
 
 export default CustomApp;
